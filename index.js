637335d94df59fe984dbbc04008afab00d0a18a4
aron k koech <varokoaruna81@gmail.com>
2024-10-29 11:56:45 +0300

    adds classes cat,dog,bird

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+class Cat {
+    constructor(name) {
+        this.name = name;
+        this.sound = 'meow'; // Default sound for Cat
+    }
+
+    speak() {
+        return `${this.name} says ${this.sound}!`;
+    }
+}
+
+class Dog {
+    constructor(name) {
+        this.name = name;
+        this.sound = 'woof'; 
+    }
+
+    speak() {
+        return `${this.name} says ${this.sound}!`;
+    }
+}
+class Bird {
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
+        this.sound = 'squawk';
+    }
+
+    speak() {
+        if (this.name === "Pablo") {
+            return `It's me! ${this.name}, the parrot!`;
+        } else {
+            return `${this.name} says ${this.sound}!`;
+        }
+    }
+}
